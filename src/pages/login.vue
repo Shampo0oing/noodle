6d3e74e4370ceeedf0a375ff21b17fbf99f0f2bb
Mohamed Fawaz <mohamedfawaz@Mohameds-MacBook-Pro.local>
2022-10-04 03:40:39 -0400

    google sign in button front end

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -75,13 +75,13 @@
             elevation="0"
             style="
               height: 45px;
-              width: 420px;
               border-radius: 8px;
               background-color: #c8e4f0;
               color: #5d4e5f;
             "
             >se connecter</v-btn
           >
+          <GoogleLogin class="gbutton" :params="params" :onSuccess="onSuccess">Se connecter avec Google</GoogleLogin>
         </div>
       </div>
     </div>
@@ -90,13 +90,22 @@
 </template>
 
 <script>
+import { GoogleLogin } from 'vue-google-login';
 export default {
+
   name: "log-in",
   data: () => ({
     username: null,
     password: null,
     wrong: false,
+    params: {
+      client_id: "459599346269-srkk8044rkjjvgeuceqoitj78v4kre9v.apps.googleusercontent.com"
+    },
   }),
+
+  components: {
+    GoogleLogin
+  },
   methods: {
     sendForm() {
       if (this.username != null || this.password != null) {
@@ -128,6 +137,12 @@ export default {
     reset() {
       this.$router.push("reset");
     },
+    onSuccess(googleUser) {
+      console.log(googleUser);
+
+      // This only gets the user information: id, name, imageUrl and email
+      console.log(googleUser.getBasicProfile());
+    }
   },
 };
 </script>
@@ -244,9 +259,48 @@ export default {
 }
 .v-text-field--outlined {
   border-radius: 8px;
-  width: 420px;
   opacity: 30%;
   color: #3f3f3f;
   background-color: #ffffff;
+}
+.gbutton {
+  transition: background-color .3s, box-shadow .3s;
+
+  padding: 12px 16px 12px 42px;
+  border: none;
+  border-radius: 8px;
+  box-shadow: 0 -1px 0 rgba(0, 0, 0, .04), 0 1px 1px rgba(0, 0, 0, .25);
+
+  color: #757575;
+  font-size: 14px;
+  font-weight: 500;
+
+  background-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4=);
+  background-color: white;
+  background-repeat: no-repeat;
+  background-position: 70px 11px;
+
+  &:hover {
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, .04), 0 2px 4px rgba(0, 0, 0, .25);
+  }
+
+  &:active {
+    background-color: #eeeeee;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow:
+        0 -1px 0 rgba(0, 0, 0, .04),
+        0 2px 4px rgba(0, 0, 0, .25),
+        0 0 0 3px #c8dafc;
+  }
+
+  &:disabled {
+    filter: grayscale(100%);
+    background-color: #ebebeb;
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, .04), 0 1px 1px rgba(0, 0, 0, .25);
+    cursor: not-allowed;
+  }
 }
 </style>
